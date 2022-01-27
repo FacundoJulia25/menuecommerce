0f3bd66d3a65286260df4a37125b547f60f26e21
FacundoJulia25 <facundodirie@gmail.com>
2022-01-27 01:23:41 -0300

    first commit

---
+++ src/components/Nav-Bar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Nav-Bar.css'
+export function NavBar() {
+    return (
+    <ul>
+        <li>Catálogo</li>
+        <li>Recetas</li>
+        <li>Toppings</li>
+    </ul>
+    )
+}
